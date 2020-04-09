fa9b98080318a07882d39c7196dd194ead16c51d
Jed Boyle <jboyle1@pca.ac.uk>
2020-04-09 10:54:09 +0100

    Added DOM selector variables that link to HTML file.

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,10 @@
 const apiKey = '548af96918134a059652040c9d8071d6';
 const url = 'https://api.rebrandly.com/v1/links';
 
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
 
   
